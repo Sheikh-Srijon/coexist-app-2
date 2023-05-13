6c7ff38158b097356d8242e4ea3d5e04e4f18cb7
JaredAzevedoSSM <jaredssm@stanford.edu>
2023-05-13 10:01:30 -0700

    Housekeeping file management for API endpoints

---
+++ coexist-app/src/api/register/route.js
@@ -0,0 +1,2 @@
+// You'll probably want to use https://nextjs.org/docs/app/building-your-application/routing/router-handlers and possibly
+// https://nextjs.org/docs/pages/building-your-application/data-fetching#getserversideprops-server-side-rendering!
