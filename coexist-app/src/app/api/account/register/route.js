ea729092ac0d33474f74958b1a332549ea481402
Alexis Echano <alexis.echano@gmail.com>
2023-05-17 16:58:21 -0700

    Fixed bugs

--- coexist-app/src/app/api/account/register/route.js
+++ coexist-app/src/app/api/account/register/route.js
@@ -6,12 +6,13 @@ export async function POST(request) {
     const db = dbClient.db("coexist_data")
     const users = db.collection("users")
 
+    const body = await request.json()
     const user = {
-        firstName: request.body.firstName,
-        lastName: request.body.lastName,
-        email: request.body.email,
-        password: request.body.password,
-        phone: request.body.phone
+        firstName: body.first,
+        lastName: body.last,
+        email: body.email,
+        password: body.password,
+        phone: body.phone
     }
 
     let result;
@@ -27,9 +28,9 @@ export async function POST(request) {
             password: user.password
         }
 
-        return NextResponse.status(201).json(data)
+        NextResponse.json({message:data}, {status: 201})
     }
     else{
-        return NextResponse.status(409)
+        NextResponse.json({}, {status: 409})
     }
 }
